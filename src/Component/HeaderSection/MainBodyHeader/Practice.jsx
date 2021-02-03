5ef3daebe9c642e004f089822416ccd15d84bc4f
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-03 18:02:59 +0530

    work

--- src/Component/HeaderSection/MainBodyHeader/Practice.jsx
+++ src/Component/HeaderSection/MainBodyHeader/Practice.jsx
@@ -33,6 +33,7 @@
             <button className="google">Sign in with Facebook <FacebookIcon/> </button>  
             <br/><br/><br/> 
             <button>sumbit</button>
+            <button>sumbit</button>
                </Form>
             </div>
             
