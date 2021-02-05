6d97714bc2e4a8b7a19629c2f89381a7c6ecd12b
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-05 15:33:11 +0530

    implemanted waset

--- src/Component/HeaderSection/MainBodyHeader/Practice.jsx
+++ src/Component/HeaderSection/MainBodyHeader/Practice.jsx
@@ -30,62 +30,6 @@
             <br/><br/>
             <button className="google">Sign in with Facebook <FacebookIcon/> </button>  
             <br/><br/><br/>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>     <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>     <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>     <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>     <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
                 </Form>
             </div>
             
