b09ccd3ac187872350fa6b1e2fd3dc2953918572
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-07 18:07:49 +0530

    checking

--- src/Component/HeaderSection/MainBodyHeader/Practice.jsx
+++ src/Component/HeaderSection/MainBodyHeader/Practice.jsx
@@ -32,21 +32,7 @@
             <br/><br/><br/> 
             <button className="google">Sign in with Facebook <FacebookIcon/> </button>  
             <br/><br/><br/> 
-            <button>login</button>
-            
-            <button>login</button>
-            
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-<button>login</button>
+  
                 </Form>
             </div>
             
