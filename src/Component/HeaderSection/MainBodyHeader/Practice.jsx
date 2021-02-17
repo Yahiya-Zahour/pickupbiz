69e5fbd605657c63029aa1b3e3013dc4814c18b7
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-17 17:08:56 +0530

    work

--- src/Component/HeaderSection/MainBodyHeader/Practice.jsx
+++ src/Component/HeaderSection/MainBodyHeader/Practice.jsx
@@ -32,7 +32,8 @@
             <br/><br/><br/> 
             <button className="google">Sign in with Facebook <FacebookIcon/> </button>  
     <br/><br/><br/>
-<button>l</button>
+<button>login</button>
+<button>login</button>
 <button>login</button>
 
             
