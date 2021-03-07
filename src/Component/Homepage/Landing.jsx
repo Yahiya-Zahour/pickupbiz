0bb6c1d2c8896fdcbd3f2ccd8fb53a2c78c1a103
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-03-07 14:25:03 +0530

    WORK

--- src/Component/Homepage/Landing.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Practice} from '../HeaderSection/MainBodyHeader/Practice';
-
- export const Landing = () => {
-    return <div>
-            <Practice/>
-            
-        </div>;
-};
